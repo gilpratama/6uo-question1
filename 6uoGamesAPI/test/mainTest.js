c234d8621cbe72d1d7f9787a4adeccb7442d6883
Gilang Pratama <gilpratama@outlook.com>
2023-09-03 12:51:19 +0700

    feat: :zap: added new unit test

--- 6uoGamesAPI/test/mainTest.js
+++ 6uoGamesAPI/test/mainTest.js
@@ -27,6 +27,51 @@ describe("Valid Login", function () {
 
 });
 
-/*
-    Implement Your Code
-*/
+describe("Invalid API", function () {
+
+    it("Handle 404 response code", function (done) {
+        request(app)
+            .post("/invalid_endpoint") // Use an invalid endpoint to trigger a 404
+            .send({
+                email: "test1@6uogames.com",
+                password: "password"
+            })
+            .expect((res) => {
+                // Add checks for the response body or other expected behavior
+                // when the response code is 404
+            })
+            .expect((res) => {
+                // Check for a response with status code 404 (Not Found)
+                if (res.status !== 404) {
+                    throw new Error("Expected status code 404");
+                }
+            })
+            .end(done);
+    });
+
+});
+
+describe("Invalid Login", function () {
+
+    it("Handle 500 response code", function (done) {
+        // Test for a response with status code 500 (Internal Server Error)
+        request(app)
+            .post("/login")
+            .send({
+                email: "error@example.com",
+                password: "invalidpassword"
+            })
+            .expect((res) => {
+                // Add checks for the response body or other expected behavior
+                // when the response code is 500
+            })
+            .expect((res) => {
+                // Check for a response with status code 500 (Internal Server Error)
+                if (res.status !== 500) {
+                    throw new Error("Expected status code 500");
+                }
+            })
+            .end(done);
+    });
+
+});
